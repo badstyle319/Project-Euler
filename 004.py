44ca02c4b5e40df599ef69439aa502e6d7a83fa5
Li-Hung Chen <badstyle319@hotmail.com>
2019-04-22 20:35:32 +0800

    1. optimized

--- 004.py
+++ 004.py
@@ -1,18 +1,24 @@
-def isPalindromic(num):
-     val = str(num)
-     i=0
-     j=len(val)-1
-     while i<=j:
-          if val[i]!=val[j]:
-               return False
-          i+=1
-          j-=1
-     return True
+import time
 
+
+def isPalindromic(num):
+    val = str(num)
+    i = 0
+    j = len(val)-1
+    while i <= j:
+        if val[i] != val[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+
+
+start = time.perf_counter()
 ans = 1
-for i in range(999,101,-1):
-     for j in range(999,101,-1):
-          num=i*j
-          if num>ans and isPalindromic(num):
-               ans = num
-print(ans)
+for i in range(999, 101, -1):
+    for j in range(999, 101, -1):
+        if i <= j:
+            num = i * j
+            if num > ans and isPalindromic(num):
+                ans = num
+print(ans, time.perf_counter()-start)
