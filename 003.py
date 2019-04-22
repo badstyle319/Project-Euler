561585e7eabbbadc58038f146b563516850e8ea0
Li-Hung Chen <badstyle319@hotmail.com>
2019-04-22 20:26:48 +0800

    1. upper value corrected

--- 003.py
+++ 003.py
@@ -6,7 +6,7 @@ TARGET = 600851475143
 start = time.perf_counter()
 ans = 0
 target = TARGET
-for i in range(3, int(math.sqrt(target) + 0.5), 2):
+for i in range(3, math.ceil(math.sqrt(target)), 2):
     if target % i == 0:
         ans = max(ans, i)
         target /= i
