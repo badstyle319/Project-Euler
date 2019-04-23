32ffe3708d51ec718456f32e2fea60f67ef12088
Li-Hung Chen <badstyle319@hotmail.com>
2019-04-23 10:02:17 +0800

    1. elapsed time added

--- 005.py
+++ 005.py
@@ -1,20 +1,22 @@
 import math
+import time
 
+TARGET = 20
+
+start = time.perf_counter()
 d = dict()
-
-for i in range(1, 21):
+for i in range(2, TARGET + 1):
     num = i
-    for j in range(2, num+1):
+    for j in range(2, num + 1):
         cnt = 0
-        while num%j==0:
-            cnt+=1
-            num/=j
+        while num % j == 0:
+            cnt += 1
+            num /= j
         if j in d:
             d[j] = max(d[j], cnt)
-        elif cnt>0:
+        elif cnt > 0:
             d[j] = cnt
-
 ans = 1
 for key in d:
     ans *= key**d[key]
-print(ans)
+print(ans, time.perf_counter()-start)
