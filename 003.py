ff9bef9d219a4c3ab31967be91c2e3fceb92c89e
Li-Hung Chen <badstyle319@hotmail.com>
2019-04-24 10:13:20 +0800

    1. without math functions

--- 003.py
+++ 003.py
@@ -1,4 +1,3 @@
-import math
 import time
 
 LIMIT = 600851475143
@@ -6,7 +5,7 @@ LIMIT = 600851475143
 start = time.perf_counter()
 ans = 0
 target = LIMIT
-for i in range(3, math.ceil(math.sqrt(target)), 2):
+for i in range(3, int(target ** 0.5) + 1, 2):
     if target % i == 0:
         ans = max(ans, i)
         target /= i
